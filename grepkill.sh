15112b94db0e4bb096827ca36ce179ec492fba3d
Wiesław Herr <herself@makhleb.net>
2011-09-08 10:21:07 +0200

    Grepkill tweaks

--- grepkill.sh
+++ grepkill.sh
@@ -13,9 +13,21 @@ then
 fi
 
 FILE=`mktemp /tmp/ps.XXXXXXXXXXXXXXXXXXXXXXXX`
+LIST=`mktemp /tmp/ps.XXXXXXXXXXXXXXXXXXXXXXXX`
 ps ax -O login > $FILE
-cat $FILE | grep -v "grepkill" | grep -i $1 | gawk 'BEGIN{i=1}{printf("%d) %s\n", i, $0); i++}'
-echo "Which process to kill?"
+cat $FILE | grep -v "grepkill" | grep -i $1 | gawk 'BEGIN{i=1}{printf("%d) %s\n", i, $0); i++}' > $LIST
+if [ `cat $LIST | wc -l` == 0 ]
+then
+	echo "No processes found!"
+	exit 1
+fi
+
+cat $LIST
+echo "Which process to kill? (please give a number, or numbers separated by spaces)"
 read DIEDIE 
-kill `cat $FILE | grep -v "grepkill" | grep $1 | sed -n "${DIEDIE}p" | gawk '{ print $1 }'`
-rm $FILE
+for NUM in $DIEDIE
+do
+	kill `cat $FILE | grep -v "grepkill" | grep $1 | sed -n "${NUM}p" | gawk '{ print $1 }'`
+done
+
+rm $FILE $LIST
