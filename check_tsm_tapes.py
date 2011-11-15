80868f7eb7ceddcffd7fb9921dd9f78a6a20599b
Wiesław Herr <herself@makhleb.net>
2011-11-15 10:09:35 +0100

    Make the alerts based on utilization, not tape numbers

--- check_tsm_tapes.py
+++ check_tsm_tapes.py
@@ -10,8 +10,8 @@ from subprocess import Popen, PIPE
 from sys import exit
 import re
 
-TRESH = 1
-UTIL_TRESH = 0.5
+UTIL_TRESH_CRIT = 90
+UTIL_TRESH_WARN = 80
 CRIT = 0
 
 CMD = "dsmadmc -id=cluster01 -password=cluster \"select STGPOOL_NAME,NUMSCRATCHUSED,MAXSCRATCH from stgpools\""
@@ -38,21 +38,23 @@ for line in ret:
 		name = m.group(1)
 		cur = m.group(2)
 		max = m.group(3)
-		if int(cur)+TRESH >= int(max) or int(cur)+CRIT >= int(max):
-			cmd_prc_fin = re.sub("_XXX_NAME_", name, CMD_PRC)
-			prg = Popen(cmd_prc_fin, stdout=PIPE, shell=True)
-			prg.wait()
-			prg.poll()
 
-			ret = prg.stdout.readline().rstrip("\n")
-			num_list = num_trap.findall(ret)
-			if len(num_list) != 0:
-				all_util = sum([float(x) for x in num_list])/len(num_list)
+		cmd_prc_fin = re.sub("_XXX_NAME_", name, CMD_PRC)
+		prg = Popen(cmd_prc_fin, stdout=PIPE, shell=True)
+		prg.wait()
+		prg.poll()
 
-		if int(cur)+CRIT >= int(max) and all_util > UTIL_TRESH:
+		ret = prg.stdout.readline().rstrip("\n")
+		num_list = num_trap.findall(ret)
+		if len(num_list) != 0:
+			all_util = sum([float(x) for x in num_list])/float(len(num_list))
+		else:
+			continue
+
+		if all_util > UTIL_TRESH_CRIT:
 			state = 2
 			out.append("CRITICAL: %s tapes: %s/%s (%d filling left, %.1f%% utilzation)" % (name, cur, max, len(num_list), all_util))
-		elif int(cur)+TRESH >= int(max) and all_util > UTIL_TRESH:
+		elif all_util > UTIL_TRESH_WARN:
 			state = 1
 			out.append("%s tapes: %s/%s (%d filling left, %.1f%% utilzation)" % (name, cur, max, len(num_list), all_util))
 
