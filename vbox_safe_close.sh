215e8da68c4c2d81840ca8a7bdfcec68b60b55e7
Wiesław Herr <herself@makhleb.net>
2012-01-12 16:41:51 +0100

    ...now for real

---
+++ vbox_safe_close.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+# Author : Wiesław Herr (herself@makhleb.net)
+# Check the included LICENSE file for licensing information
+#
+# Saves the state of all running Virtualbox machines
+# I always forget to do that when I halt the system, so aliased halt for
+# vbox_safe_close.sh; halt
+##############
+
+VM=$(VBoxManage list runningvms)
+if [[ `echo "$VM" | wc -l` -eq 0 ]]
+then
+	echo "No running VMs detected"
+	exit
+fi
+
+for i in `echo "$VM" | awk '{ print $ 1 }' | sed 's/"//g'`
+do
+	VBoxManage controlvm $i savestate
+done
